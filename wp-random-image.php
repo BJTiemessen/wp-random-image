c8842432d14d4360f5d99bed18e6e2f63ec62a39
BJ Tiemessen <bj.tiemessen@gmail.com>
2015-05-25 16:56:03 -0500

    Initial
    Initial commit of wp-random-images.php.  This is the main class of the
    plugin.  I added the comments section that define the plugin.

---
+++ wp-random-image.php
@@ -0,0 +1,10 @@
+<?php
+/*******************************************************************************
+* Plugin name: WP Random Product Image
+* Plugin URI: http://www.bjtiemessen.com/
+* Description: A plugin that displays a random product image.  Designed to be used for displaying featured, new, or on sale products.
+* Author: BJ Tiemessen
+* Author URI: http://www.bjtiemessen.com
+* Version: 1.0
+* Text Domain: wp-random-image
+********************************************************************************/
