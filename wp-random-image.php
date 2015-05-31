9ac030a07382f57f95281e24b67217346a46c41e
BJ Tiemessen <bj.tiemessen@gmail.com>
2015-05-30 23:55:07 -0500

    Version 1
    README: Added markdown tags, added more description and installation
    instructions.
    
    wp-random-image.php: Wrote class WPRandomImage which reads in the images
    in the images folder into an array.  Then added the shortcode function
    that grabs a random image from the array and displays it.

--- wp-random-image.php
+++ wp-random-image.php
@@ -1,6 +1,6 @@
 <?php
 /*******************************************************************************
-* Plugin name: WP Random Product Image
+* Plugin name: WP Random Image
 * Plugin URI: http://www.bjtiemessen.com/
 * Description: A plugin that displays a random product image.  Designed to be used for displaying featured, new, or on sale products.
 * Author: BJ Tiemessen
@@ -8,3 +8,60 @@
 * Version: 1.0
 * Text Domain: wp-random-image
 ********************************************************************************/
+
+
+/**
+*  Main class of our plugin
+*/
+class WPRandomImage
+{
+	//Create an array to store the paths of the pictures in
+	public $pic_paths = array();
+	
+	/****************************
+	* Constructor
+	* Read in the the list of files to an array so they are ready for use by the other functions.
+	* @TODO: Make this read from the Media Library so that you can easily make changes instead of having to upload pictures to a special directory
+	*****************************/
+	function __construct()
+	{
+		$dir = plugin_dir_path(__FILE__).'images/';
+		$dirhandle = opendir($dir);
+		if(is_dir($dir))
+		{
+			if($dirhandle = opendir($dir))
+			{
+				while(($filename = readdir($dirhandle)) !== false)
+				{
+					if($filename != "." && $filename != "..")
+					{
+						//$this->pic_paths[] = $dir.$filename;
+						$this->pic_paths[] = $filename;
+					}
+				}
+				closedir($dirhandle);
+			}
+		}
+		//print_r($this->pic_paths);
+	}
+
+	/****************************
+	* add_hooks: called to to setup and hook into wp for our class
+	*****************************/
+	public function add_hooks()
+	{
+
+	}
+
+	public function random_image_shortcode()
+	{
+		$imageurl = plugin_dir_url(__FILE__).$this->pic_paths[mt_rand(0, count($this->pic_paths)-1)];
+		$output = '<div class=wp-random-image>';
+		$output .= '<img src="'.$imageurl.'" />';
+		$output .= '</div>';
+		return $output;
+	}
+}
+
+$wpri_random_image = new WPRandomImage();
+add_shortcode('wprandomimg', array($wpri_random_image, 'random_image_shortcode'));
